d479653446cb99f8fcb01283678ac23d89cd3ee7
Cody Miller <cmiller221@gmail.com>
2021-05-01 16:05:44 -0500

    Initial Commit

---
+++ MarvelPlayground/MarvelPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelPlaygroundApp.swift
+//  MarvelPlayground
+//
+//  Created by Cody Miller on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
